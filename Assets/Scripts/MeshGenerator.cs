43e9d7a19b17e50e27a69dc050aafd0b08a1f160
Glauber Gualda <Glauber Gualda>
2016-02-04 17:58:36 -0200

    aula 9 part 1

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -5,7 +5,9 @@ public class MeshGenerator : MonoBehaviour {
 
     public float wallHeight;
     public MeshFilter walls;
+    public MeshFilter cave;
     public SquareGrid squareGrid;
+    public bool is2D;
     List<Vector3> vertices;
     List<int> triangles;
 
@@ -32,13 +34,14 @@ public class MeshGenerator : MonoBehaviour {
         }
 
         Mesh mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+        cave.mesh = mesh;
 
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.RecalculateNormals();
 
-        CreateWallMesh();
+        if(!is2D)
+            CreateWallMesh();
     }
 
     void CreateWallMesh()
@@ -71,6 +74,9 @@ public class MeshGenerator : MonoBehaviour {
         wallMesh.vertices = wallVertices.ToArray();
         wallMesh.triangles = wallTriangles.ToArray();
         walls.mesh = wallMesh;
+
+        MeshCollider wallCollider = walls.gameObject.AddComponent<MeshCollider>();
+        wallCollider.sharedMesh = wallMesh;
     }
 
 
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody rb;
+    Vector3 velocity;
+
+	
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical")).normalized * 10;
+
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	
+	}
+}
