92725f8427462fde57f9f3962ac2aa9c343f22ae
Glauber Gualda <Glauber Gualda>
2016-02-05 17:53:55 -0200

    aula 9 part 2

---
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement2D : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Vector2 velocity;
+
+	
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () {
+        velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * 10;
+
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+	
+	}
+}
